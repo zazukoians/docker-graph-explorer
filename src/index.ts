0c247bafc42eb4d01f545108719058e04535e2d5
Ludovic Muller <ludovicmuller1@gmail.com>
2025-12-09 18:34:27 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import Fastify from "fastify";
+import pointOfView from "@fastify/view";
+import fastifyStatic from "@fastify/static";
+
+import handlebars from "handlebars";
+
+import { resolve } from "import-meta-resolve";
+
+// Server configuration
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const host = process.env.HOST || "0.0.0.0";
+
+// Graph Explorer configuration
+const endpointUrl = process.env.SPARQL_ENDPOINT || "https://dbpedia.org/sparql";
+const acceptBlankNodes = process.env.ACCEPT_BLANK_NODES === "true" || false;
+const dataLabelProperty = process.env.DATA_LABEL_PROPERTY || "rdfs:label";
+const schemaLabelProperty = process.env.SCHEMA_LABEL_PROPERTY || "rdfs:label";
+const language = process.env.LANGUAGE || "en";
+const languages = process.env.LANGUAGES
+  ? JSON.parse(process.env.LANGUAGES)
+  : [
+      { code: "en", label: "English" },
+      { code: "de", label: "German" },
+      { code: "fr", label: "French" },
+      { code: "it", label: "Italian" },
+    ];
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const server = Fastify({
+  logger: true,
+});
+
+const distPath = resolve("graph-explorer/dist/", import.meta.url);
+server.register(fastifyStatic, {
+  root: distPath.replace(/^file:\/\//, ""),
+  prefix: "/graph-explorer/assets/",
+  decorateReply: false,
+});
+server.register(fastifyStatic, {
+  root: `${currentDir}/static/`,
+  prefix: "/graph-explorer/static/",
+  decorateReply: false,
+});
+
+server.register(pointOfView, {
+  engine: {
+    handlebars,
+  },
+  root: join("src", "views"),
+  layout: join("templates", "default.hbs"),
+  viewExt: "hbs",
+});
+
+server.get("/healthz", async (request, reply) => {
+  reply.type("text/plain").send("OK");
+});
+
+server.get("/", async (request, reply) => {
+  return reply.view("graph-explorer.hbs", {
+    // Just forward all the config as a string
+    graphExplorerConfig: JSON.stringify({
+      endpointUrl,
+      acceptBlankNodes,
+      dataLabelProperty,
+      schemaLabelProperty,
+      language,
+      languages,
+    }).replace(/'/g, "\\'"),
+  });
+});
+
+server.listen({ port, host }, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
---
+++ src/static/app.js
@@ -0,0 +1,70 @@
+/* global ReactDOM, React, GraphExplorer, graphExplorerConfig */
+
+const SparqlDialect = GraphExplorer.OWLStatsSettings
+SparqlDialect.dataLabelProperty = graphExplorerConfig.dataLabelProperty
+SparqlDialect.schemaLabelProperty = graphExplorerConfig.schemaLabelProperty
+
+const onWorkspaceMounted = async (workspace) => {
+  if (!workspace) {
+    return
+  }
+
+  const model = workspace.getModel()
+
+  model.importLayout({
+    dataProvider: new GraphExplorer.SparqlDataProvider(
+      {
+        endpointUrl: graphExplorerConfig.endpointUrl,
+        acceptBlankNodes: graphExplorerConfig.acceptBlankNodes,
+        imagePropertyUris: ['http://xmlns.com/foaf/0.1/img'],
+        queryMethod: GraphExplorer.SparqlQueryMethod.GET,
+      },
+      SparqlDialect,
+    ),
+  })
+
+  /**
+   * get the '?resources' search param and load those resources
+   */
+  const url = new URL(window.location.href)
+  const resources = url.searchParams.get('resources')
+
+  if (resources) {
+    const elm = await model.dataProvider.elementInfo({
+      elementIds: resources.split(';'),
+    })
+
+    const elmIds = []
+    resources.split(';').forEach((item) => {
+      const node = model.createElement(elm[item])
+      elmIds[item] = node.id
+      workspace.forceLayout()
+    })
+
+    /* now that we have the resources, add the links */
+    const lnk = await model.dataProvider.linksInfo({
+      elementIds: resources.split(';'),
+    })
+
+    lnk.forEach((link) => {
+      const newLink = new GraphExplorer.Link({
+        typeId: link.linkTypeId,
+        sourceId: elmIds[link.sourceId],
+        targetId: elmIds[link.targetId],
+      })
+      model.addLink(newLink)
+      workspace.forceLayout()
+    })
+  }
+}
+
+const props = {
+  ref: onWorkspaceMounted,
+  languages: graphExplorerConfig.languages,
+  language: graphExplorerConfig.language,
+}
+
+ReactDOM.render(
+  React.createElement(GraphExplorer.Workspace, props),
+  document.getElementById('graph-explorer-instance'),
+)
